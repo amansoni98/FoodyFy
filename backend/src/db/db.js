d945ce8d530913de15e4eb8a657a2e8bc528c3bd
amansoni98 <arjunsoniaman@gmail.com>
2026-06-02 21:28:27 +0530

    Secured hide

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 
 function connectDB() {
   mongoose
-    .connect("mongodb://foodyfy:aman9893@ac-afbvrpa-shard-00-00.yslvgmf.mongodb.net:27017,ac-afbvrpa-shard-00-01.yslvgmf.mongodb.net:27017,ac-afbvrpa-shard-00-02.yslvgmf.mongodb.net:27017/?ssl=true&replicaSet=atlas-15fatf-shard-0&authSource=admin&appName=FoodyFy")
+    mongoose.connect(process.env.MONGO_URI)
     .then(() => {
       console.log("Mongodb connected successfully....");
     })
